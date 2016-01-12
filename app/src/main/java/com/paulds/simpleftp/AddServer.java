3a28f821abb3a080c1d7144bc20fe9aed168db07
Paul-DS <Paul-DS@users.noreply.github.com>
2016-01-13 00:19:05 +0100

    Initialize android project

---
+++ app/src/main/java/com/paulds/simpleftp/AddServer.java
@@ -0,0 +1,13 @@
+package com.paulds.simpleftp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddServer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_server);
+    }
+}
---
+++ app/src/main/java/com/paulds/simpleftp/entities/FtpServer.java
@@ -0,0 +1,12 @@
+package com.paulds.simpleftp.entities;
+
+/**
+ * Created by Paul on 13/01/2016.
+ */
+public class FtpServer {
+    private int Id;
+    private string Host;
+    private boolean IsAnonymous;
+    private string Login;
+    private string Password;
+}
